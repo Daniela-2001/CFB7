7e123d69b8495e5d983c7f4362e51e94c11908b7
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-16 18:58:44 +0000

    CFB7

---
+++ aula07/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace aula07
+{
+    internal class Program
+    {
+        static void Main(){
+
+            const string canal = "CFB Cursos";
+
+            const double pi = 3.1415;
+
+            Console.WriteLine("Canal:{0}\nPi: {1}", canal,pi);
+        }
+    }
+}
